b5e6fd39005108e1d0fdb01720475ece1f1fdd02
mizza-cz <mizza.cz666@gmail.com>
2021-09-29 18:04:00 +0200

    add archiv/fotogalelrie

--- app/js/main.js
+++ app/js/main.js
@@ -111,7 +111,50 @@ $(function(){
 
  });
    // $('select, input.radio, input.checkbox').styler();
- 
+   $('.main__filtr .tab').on('click', function (event) {
+      var id = $(this).attr('data-id');
+      $('.main__filtr').find('.category').removeClass('tab-active').hide();
+      $('.main__filtr .main__tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#' + id)
+        .addClass('tab-active')
+        .fadeIn();
+      return false;
+    });
+    var _gallery_loading = false;
 
+    $('a[data-gallery]').click(function () {
+      if (_gallery_loading) return;
+      _gallery_loading = true;
+    
+      var mode = $(this).data('gallery');
+      var url = '/inc/gallery_ajax.asp?mode=' + mode;
+    
+      if (mode == 1 || mode == 2) {
+        url += '&id=' + $(this).data('gallery-id');
+      }
+    
+      $.getJSON(url, { format: 'json' })
+        .done(function (e) {
+          $(this).lightGallery({
+            hash: false,
+            share: false,
+            dynamic: true,
+            dynamicEl: e,
+            download: false,
+            backdropDuration: 500,
+          });
+        })
+        .fail(function (e, textStatus, error) {
+          alert('Nastala chyba při načítání galerie. Prosím zkuste to znovu.');
+          console.error('getJSON failed, status: ' + textStatus + ', error: ' + error);
+          console.error(e);
+        })
+        .always(function () {
+          _gallery_loading = false;
+        });
+    
+      return false;
+    });
 // end
 });
